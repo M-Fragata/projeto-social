8405515b623e2cc80cbd9683422c78406c15eb2a
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-09-21 11:46:43 -0300

    ATUALIZAÇÃO DE PASTAS

---
+++ script/script.js
@@ -0,0 +1,19 @@
+        function home() {
+            document.querySelector('iframe#tela').src = "paginas/home.html";
+        }
+
+        function youtube() {
+            document.querySelector('iframe#tela').src = "paginas/youtube.html";
+        }
+        function github() {
+            document.querySelector('iframe#tela').src = "paginas/github.html";
+        }
+        function instagram() {
+            document.querySelector('iframe#tela').src = "paginas/instagram.html";
+        }
+        function twitter() {
+            document.querySelector('iframe#tela').src = "paginas/twitter.html";
+        }
+        function facebook() {
+            document.querySelector('iframe#tela').src = "paginas/facebook.html";
+        }
